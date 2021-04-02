9976eb58b899b7cad0ac9726cf2eaf8aca531caf
symac <web@geobib.fr>
2021-04-02 16:21:19 +0200

    amélioration des stats avec intégration du nombre d'erreur plus possibilité d'export.

--- src/Controller/IlnController.php
+++ src/Controller/IlnController.php
@@ -6,9 +6,15 @@ namespace App\Controller;
 
 use App\Entity\BatchImport;
 use App\Entity\Iln;
+use App\Entity\Record;
 use App\Repository\IlnRepository;
 use App\Repository\RecordRepository;
 use App\Service\AbesLanguages;
+use Box\Spout\Common\Entity\Style\Style;
+use Box\Spout\Common\Type;
+use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Writer\Common\Creator\WriterFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -49,6 +55,93 @@ class IlnController extends AbstractController
     {
         $stats = $this->getDoctrine()->getRepository(Iln::class)->getStats($iln);
         return $this->render("iln/iln_stats.html.twig", ["iln" => $iln, "stats" => $stats]);
+    }
+
+    /**
+     * @Route("/chantier/{code}-{secret}/stats/export/{format}", name="export_iln_stats")
+     */
+    public function ilnExportStats(Iln $iln, string $format)
+    {
+        if ($format == "xlsx") {
+            $type = Type::XLSX;
+            $contentType = "application/vnd.ms-excel";
+        } elseif ($format == "ods") {
+            $type = Type::ODS;
+            $contentType = "application/vnd.oasis.opendocument.spreadsheet";
+        } elseif ($format == "csv") {
+            $type = Type::CSV;
+            $contentType = "text/csv";
+        } else {
+            return new Response("Format d'export " . $format . " inconnu");
+        }
+
+        $styleBold = (new StyleBuilder())->setFontBold()->build();
+
+        $outputFilename = "stats-iln" . $iln->getNumber() . "-" . date("Ymd") . "." . $format;
+
+        $tmpFile = sys_get_temp_dir() . '/' . uniqid(time() * rand(1, 999)) . '.xlsx';
+        $writer = WriterFactory::createFromType($type);
+        $writer->openToFile($tmpFile);
+
+        $header = [
+            WriterEntityFactory::createCell('Annéee'),
+            WriterEntityFactory::createCell('Mois'),
+            WriterEntityFactory::createCell('Jour'),
+            WriterEntityFactory::createCell('Corrigés - Notices'),
+            WriterEntityFactory::createCell('Corrigés - Liens'),
+            WriterEntityFactory::createCell('À reprendre - Notices'),
+            WriterEntityFactory::createCell('À reprendre - Liens'),
+        ];
+
+        $writer->addRow(WriterEntityFactory::createRow($header, $styleBold));
+        $stats = $this->getDoctrine()->getRepository(Iln::class)->getStats($iln);
+
+        $total = [
+            "nbrecordsvalidated" => 0,
+            "nberrorsvalidated" => 0,
+            "nbrecordsskipped" => 0,
+            "nberrorsskipped" => 0,
+        ];
+
+        foreach ($stats as $year => $subtable) {
+            foreach ($subtable as $month => $subsubtable) {
+                foreach ($subsubtable as $day => $values) {
+                    $row = [
+                        WriterEntityFactory::createCell($year),
+                        WriterEntityFactory::createCell($month),
+                        WriterEntityFactory::createCell($day),
+                        WriterEntityFactory::createCell(intval($values[Record::RECORD_VALIDATED]["nbrecords"])),
+                        WriterEntityFactory::createCell(intval($values[Record::RECORD_VALIDATED]["nberrors"])),
+                        WriterEntityFactory::createCell(intval($values[Record::RECORD_SKIPPED]["nbrecords"])),
+                        WriterEntityFactory::createCell(intval($values[Record::RECORD_SKIPPED]["nberrors"])),
+                    ];
+                    $writer->addRow(WriterEntityFactory::createRow($row));
+
+                    $total["nbrecordsvalidated"] += intval($values[Record::RECORD_VALIDATED]["nbrecords"]);
+                    $total["nberrorsvalidated"] += intval($values[Record::RECORD_VALIDATED]["nberrors"]);
+                    $total["nbrecordsskipped"] += intval($values[Record::RECORD_SKIPPED]["nbrecords"]);
+                    $total["nberrorsskipped"] += intval($values[Record::RECORD_SKIPPED]["nberrors"]);
+                }
+            }
+        }
+
+        $rowTotal = [
+            WriterEntityFactory::createCell(null),
+            WriterEntityFactory::createCell(null),
+            WriterEntityFactory::createCell("Total"),
+            WriterEntityFactory::createCell($total["nbrecordsvalidated"]),
+            WriterEntityFactory::createCell($total["nberrorsvalidated"]),
+            WriterEntityFactory::createCell($total["nbrecordsskipped"]),
+            WriterEntityFactory::createCell($total["nberrorsskipped"]),
+        ];
+        $writer->addRow(WriterEntityFactory::createRow($rowTotal, $styleBold));
+        $writer->close();
+
+        return new Response(file_get_contents($tmpFile), 200,
+            array(
+                'Content-Type' => $contentType,
+                'Content-Disposition' => 'attachment; filename="'.$outputFilename.'"',
+            ));
     }
 
 
@@ -113,7 +206,7 @@ class IlnController extends AbstractController
                 $session = $request->getSession();
                 $session->getFlashBag()->add('danger', "Le PPN <strong>$ppn</strong> n'a pas été trouvé pour ce chantier.");
                 return $this->redirect(
-                  $this->generateUrl("view_iln", ["code" => $iln->getCode(), "secret" => $iln->getSecret()])
+                    $this->generateUrl("view_iln", ["code" => $iln->getCode(), "secret" => $iln->getSecret()])
                 );
             }
         }
@@ -127,10 +220,10 @@ class IlnController extends AbstractController
      */
     public function exportBatchImport(BatchImport $batchImport)
     {
-        $filename = "export_batch_".$batchImport->getId().".txt";
+        $filename = "export_batch_" . $batchImport->getId() . ".txt";
         $content = "";
         foreach ($batchImport->getRecords() as $record) {
-            $content .= $record->getPpn()."\n";
+            $content .= $record->getPpn() . "\n";
         }
 
         $response = new Response($content);
--- src/Repository/IlnRepository.php
+++ src/Repository/IlnRepository.php
@@ -3,7 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Iln;
-use App\Entity\Rcr;
+use App\Entity\Record;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -22,27 +22,33 @@ class IlnRepository extends ServiceEntityRepository
 
     public function getStats(Iln $iln)
     {
-        $rcrs = $this->getEntityManager()->getRepository(Rcr::class)->findByIln($iln);
-
         // Pour faire la disctinction par RCR, non utilisé pour le moment;
-        $sql = "SELECT rcr_create_id, DATE_FORMAT(updated_at, '%Y') as year, DATE_FORMAT(updated_at, '%m') as month, DAYOFMONTH(updated_at) as day, status, count(*) as nbrecords FROM `record` where status in (1, 2) and rcr_create_id in (select id from rcr where rcr.iln_id = ?) group by rcr_create_id, year, month, day, status";
-        $sql = "SELECT rcr_create_id, DATE_FORMAT(updated_at, '%Y') as year, DATE_FORMAT(updated_at, '%m') as month, DAYOFMONTH(updated_at) as day, status, count(*) as nbrecords FROM `record` where status in (1, 2) and rcr_create_id in (select id from rcr where rcr.iln_id = ?) group by rcr_create_id, DATE_FORMAT(updated_at, '%Y'), month, day, status order by year, month, day";
+        $sql = "SELECT DATE_FORMAT(updated_at, '%Y') as year, DATE_FORMAT(updated_at, '%m') as month, DAYOFMONTH(updated_at) as day, status, count(distinct record.id) as nbrecords, count(distinct link_error.id) as nberrors FROM `record`, `link_error` where link_error.record_id = record.id and status in (" . Record::RECORD_VALIDATED . ", " . Record::RECORD_SKIPPED . ") and rcr_create_id in (select id from rcr where rcr.iln_id = ?) group by DATE_FORMAT(updated_at, '%Y'), month, day, status order by year, month, day";
 
         $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
         $stmt->bindValue(1, $iln->getId());
         $stmt->execute();
 
         $output = [];
-        $results = $stmt->fetchAll();
+        $results = $stmt->fetchAllAssociative();
         foreach ($results as $result) {
 
-            if (!isset($output[$result["year"]])) { $output[$result["year"]] = [];}
-            if (!isset($output[$result["year"]][$result["month"]])) { $output[$result["year"]][$result["month"]] = [];}
-            if (!isset($output[$result["year"]][$result["month"]][$result["day"]])) { $output[$result["year"]][$result["month"]][$result["day"]] = ["1" => 0, "2" => 0];}
+            if (!isset($output[$result["year"]])) {
+                $output[$result["year"]] = [];
+            }
+            if (!isset($output[$result["year"]][$result["month"]])) {
+                $output[$result["year"]][$result["month"]] = [];
+            }
+            if (!isset($output[$result["year"]][$result["month"]][$result["day"]])) {
+                $output[$result["year"]][$result["month"]][$result["day"]] = [Record::RECORD_VALIDATED => ["nbrecords" => 0, "nberrors" => 0], Record::RECORD_SKIPPED => ["nbrecords" => 0, "nberrors" => 0]];
+            }
 
-            $output[$result["year"]][$result["month"]][$result["day"]][$result["status"]] += $result["nbrecords"];
-
+            $output[$result["year"]][$result["month"]][$result["day"]][$result["status"]] = [
+                "nbrecords" => $result["nbrecords"],
+                "nberrors" => $result["nberrors"]
+            ];
         }
+
         return $output;
     }
 
